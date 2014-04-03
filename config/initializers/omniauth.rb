cd5655488e722e6032e50787aab2823d77f0ec63
Max Harris <mjharris@colorado.edu>
2014-04-02 21:24:18 -0600

    add redirect to test app when on localhost

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '561983067232884', '9e4cbd261a276a7ae828433726ab402f'
+  
+  if Rails.env == "development" || Rails.env == "test"
+    provider :facebook, "734159986623825", "fb44d7bca8dccb0403f01edc0044556d"
+  else
+    provider :facebook, "561983067232884", "9e4cbd261a276a7ae828433726ab402f"
+  end
 end

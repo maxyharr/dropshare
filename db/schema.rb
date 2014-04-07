54c590af96b9f6ebed2c43ed2ec511dc465d53c0
Max Harris <mjharris@colorado.edu>
2014-04-07 15:42:52 -0600

    add pulled commits

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140331234849) do
 
-  create_table "sessions", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"

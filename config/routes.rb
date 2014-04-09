e006536d926833201d05a6a98c3911659d5b1f26
Max Harris <mjharris@colorado.edu>
2014-04-08 21:38:45 -0600

    edit routes - no actual change

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Dropshare::Application.routes.draw do
   get "drive/download"
   get "drive/logout"
   
+  get "/oauth/authorize"
+  
   # To link facebook 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]

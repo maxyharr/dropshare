2ec123b3c74544bdf0bd7b09efdb87dcad30689a
vyruz <vyruz@github.com>
2014-04-12 12:59:04 -0600

    add all files

--- db/migrate/20140331234849_create_users.rb
+++ db/migrate/20140331234849_create_users.rb
@@ -1,21 +1,29 @@
 class CreateUsers < ActiveRecord::Migration
-  def up
-    create_table 'users' do |t|
-      t.string   "name"
-      t.string   "email"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.string   "facebook"
-      t.string   "uid"
-      t.string   "provider"
-      t.string   "oauth_token"
-      t.datetime "oauth_expires_at"
+#  def up
+#    create_table 'users' do |t|
+#      t.string   "name"
+#      t.string   "email"
+#      t.datetime "created_at"
+#      t.datetime "updated_at"
+#     t.string   "facebook"
+#      t.string   "uid"
+#     t.string   "provider"
+#     t.string   "oauth_token"
+#     t.datetime "oauth_expires_at"
+#   end
+#  end
+ 
+  def change
+    create_table :users do |t|
+      t.string :identifier
+      t.string :access_token
+
+      t.timestamps
     end
   end
   
-  
-  def down
-    drop_table 'users'
-  end
+#  def down
+#    drop_table 'users'
+#  end
   
 end

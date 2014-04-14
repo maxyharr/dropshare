1ac2ab9bd8c8c1fa5248f3272a2093fa455b2692
Max Harris <mjharris@colorado.edu>
2014-04-14 15:58:20 -0600

    add signin

--- features/step_definitions/signin_steps.rb
+++ features/step_definitions/signin_steps.rb
@@ -3,7 +3,7 @@ Given(/^I am signed in with facebook/) do
   click_link("sign_in")
 end
 
-# When(/^I sign in with facebook$/) do
-#   visit root_path
-#   click_link("sign_in")
-# end
+When(/^I sign in with facebook$/) do
+  visit root_path
+  click_link("sign_in")
+end

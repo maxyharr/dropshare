e965ed0b9213dc264c44b6cb65a751462f39a831
vyruz <vyruz@github.com>
2014-04-17 11:51:33 -0600

    trying to get mock working

--- features/step_definitions/signin_steps.rb
+++ features/step_definitions/signin_steps.rb
@@ -1,9 +1,12 @@
 Given(/^I am signed in with facebook/) do
-  visit root_path
-  click_link("sign in")
+  mock_graph :get, 'me', 'users/me_private', :access_token => 'access_token' do
+    user = FbGraph::User.me('access_token').fetch
+  end
 end
 
 When(/^I sign in with facebook$/) do
   visit root_path
-  click_link("sign in")
+  mock_graph :get, 'me', 'users/me_private', :access_token => 'access_token' do
+    user = FbGraph::User.me('access_token').fetch
+  end
 end

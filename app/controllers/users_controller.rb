22980079a996c825bdf0d16c8a66c30a2d1953a1
Max Harris <mjharris@colorado.edu>
2014-04-21 09:18:49 -0600

    revert to working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def feed
     redirect_to root_path if current_user == nil
+    @graph_user = FbGraph::User.me(current_user.oauth_token)
+    @friends = @graph_user.friends
+    
     # cache the user's friends (at least for now - long render time otherwise ~4sec)
   end
   

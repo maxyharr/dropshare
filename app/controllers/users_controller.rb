d0322a01807869ca128f7a8e2fc27c8363c1a6d8
Max Harris <mjharris@colorado.edu>
2014-04-20 23:42:50 -0600

    show only friends that have dropshare

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
+  
   def feed
     redirect_to root_path if current_user == nil
+    @dropshare_friends = Hash.new
     @graph_user = FbGraph::User.me(current_user.oauth_token)
     @friends = @graph_user.friends
+    
+    # cache the user's friends (at least for now - long render time otherwise ~4sec)
+    if @dropshare_friends == nil
+      @friends.each do |friend|
+        dropshare_user = User.find_by_uid(friend.identifier)
+        @dropshare_friends + dropshare_user if dropshare_user != nil
+      end
+    end
   end
   
   def wall

697967635873774a61645fd0a907228844332959
vyruz <vyruz@github.com>
2014-04-21 16:17:17 -0600

    FbGraph new user spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe UsersController do
-
+  describe '.feed' do
+    it 'should setup all supported attributes' do
+      attributes = {
+        :id => '12345',
+        :name => "Luke Woodruff",
+        :friends => "friend1, someOtherPerson"
+      }
+      user = FbGraph::User.new(attributes.delete(:id),attributes)
+      user.name.should == "Luke Woodruff"
+    end
+  end
 end

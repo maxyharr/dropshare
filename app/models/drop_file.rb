55c45c9d3c865051cf55fda17c06e395f0467524
Max Harris <mjharris@colorado.edu>
2014-04-22 18:14:45 -0600

    add some drop_file unit tests

--- app/models/drop_file.rb
+++ app/models/drop_file.rb
@@ -1,6 +1,7 @@
 class DropFile < ActiveRecord::Base
   belongs_to :user
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
-    validates :name, presence: true # Make sure the file's name is present.
-    validate :user_id, presence: true # Make sure we can associate the file with a user
+    validates :name, presence: true, length: { maximum: 50 } # Make sure the file's name is present and not too long.
+    validates :user_id, presence: true # Make sure we can associate the file with a user
+    validates :attachment, presence: true
 end
--- spec/models/drop_file_spec.rb
+++ spec/models/drop_file_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe DropFile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @drop_file = DropFile.new(name: "Test File", user_id: 1, attachment: "test_file.extension") }
+  
+  subject { @drop_file }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:user_id) }
+  it { should respond_to(:name) }
+  it { should respond_to(:created_at) }
+  it { should respond_to(:updated_at) }
+  
+  it { should_not respond_to(:foobar) }
+  
+  describe "when name is not present" do
+    before { @drop_file.name = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when user_id is not present" do
+    before { @drop_file.user_id = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when attachment is not present" do
+    before { @drop_file.attachment = "" }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @drop_file.name = "a" * 12345 }
+    it { should_not be_valid }
+  end
 end
